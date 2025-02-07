39b231c5483fdba163c25db60001910e6a85487f
alanememichael9@gmail.com <alanememichael9@gmail.com>
2025-02-07 16:24:43 +0100

    just html and css upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//Validating numbers of people
+
+const error = () => {
+    const numOfPeople = document.getElementById('numOfPeople');
+    numOfPeople.classList.add('errorAction');
+
+    const errorMessage = document.querySelector('.errorMessage');
+    if(errorMessage) {
+        errorMessage.style.display = "block";
+    }
+}
+
+
+function validateNumOfPeople(input) {
+   input.value = input.value.replace(/[^0-9.]/g, '');
+    
+   
+   if(parseInt(input.value === '0')) {
+       error();
+   } else {
+    document.getElementById('numOfPeople').classList.remove('errorAction');
+    errorMessage.style.display = "none";
+   }
+}
+
+document.getElementById('numOfPeople').addEventListener('input', validateNumOfPeople(this));
